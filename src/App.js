ac5e0132ec0764135df47e3489d1dcb77b5715cd
deekshithmd <deekshithmogra@gmail.com>
2024-02-12 22:06:59 +0530

    first react ssr app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+  return (
+    <div className="App">
+      <h3>{count}</h3>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
